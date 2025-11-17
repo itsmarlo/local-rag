38b602f3532c64e312039bc1164344e20919cdc0
Marla 🌱 <itsmarlo.tech@gmail.com>
2025-11-17 15:27:44 +0100

    fix  env

--- populate_database.py
+++ populate_database.py
@@ -1,11 +1,12 @@
 import argparse
 import os
 import shutil
-from langchain.document_loaders.pdf import PyPDFDirectoryLoader #loads all PDFs in a directory
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.vectorstores import Chroma
 from langchain.schema.document import Document
 from get_embedding_function import get_embedding_function
 from langchain.vectorstores.chroma import Chroma #importing chroma vector  store
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 CHROMA_PATH = "chroma"
 DATA_PATH = "data"
